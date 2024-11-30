8f0e4e895669ffa6677ade574ebed3408205de71
mochrks <mochrizkiks@gmail.com>
2024-11-30 22:34:00 +0700

    feat: add copyright

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,20 @@ import UploadFile from "@/components/UploadFile";
 export default function Home() {
 
   return (
-    <div className="w-full">
+    <div className="w-full h-screen flex flex-col justify-beetween " >
       {/* <div className="py-5">
         <ExampleAI />
       </div> */}
-      <UploadFile />
+      <div className="flex-1">
+        <UploadFile />
+      </div>
+      <footer className="flex items-end w-full ">
+        <div className="container mx-auto flex justify-center items-center py-4">
+          <p className="text-sm text-muted-foreground text-center">
+            © {new Date().getFullYear()} All rights reserved by <a href="https://github.com/Mochrks" className="hover:underline">@mochrks</a>
+          </p>
+        </div>
+      </footer>
     </div>
   );
 }
